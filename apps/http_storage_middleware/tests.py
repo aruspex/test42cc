fd8b95ecbe82738c1e2e173f5a586a8c9a63650a
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-12 12:42:14 +0300

    ticket:3 added tests

---
+++ apps/http_storage_middleware/tests.py
@@ -0,0 +1,27 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from .models import HttpRequestInfo
+
+
+class HttpMiddlewareTest(TestCase):
+    fixtures = ['contact/fixtures/initial_data.json']
+
+    def setUp(self):
+        self.url = reverse('requests')
+
+    def test_requests_are_properly_add_to_db(self):
+        for request_index in xrange(1, 4):
+            self.client.get(self.url)
+            saved_requests_count = HttpRequestInfo.objects.count()
+            self.assertEqual(saved_requests_count, request_index)
+
+    def test_view_for_get_and_post_requests(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Method', response.content)
+        # POST appears in body after POST request
+        self.assertNotIn('POST', response.content)
+        self.client.post(self.url)
+        response = self.client.get(self.url)
+        self.assertIn('POST', response.content)
