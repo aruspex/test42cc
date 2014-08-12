beb06f3b870e7424d930209a7d965c12ab67138e
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-12 10:02:10 +0300

    hello world functionality

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'apps.hello.views.hello', name='hello'),
     url(r'^admin/', include(admin.site.urls)),
 )
