951c3d211aff7d3b9f9dec97205e7b9ac170c2da
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-12 10:16:58 +0300

    trying to serve static files on getbarista

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,9 +1,19 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.hello.views.hello', name='hello'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
+urlpatterns += staticfiles_urlpatterns()
