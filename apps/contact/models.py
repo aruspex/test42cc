9f36ee4ed7be2620385bf2a8194e272eb8b4a5d8
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-12 13:21:53 +0300

    ticket:5 added verbose names to some Person fields

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=25)
-    birth_date = models.DateField()
+    surname = models.CharField(max_length=25, verbose_name=u'Last name')
+    birth_date = models.DateField(verbose_name=u'Date of birth')
     bio = models.TextField()
     email = models.EmailField()
     jabber = models.EmailField()
