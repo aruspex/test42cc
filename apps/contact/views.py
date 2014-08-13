4df24dade8c95542f3353f31d937a4e019857542
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-13 19:14:40 +0300

    ticket:13 different bugfixes

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import PersonForm
@@ -14,6 +15,13 @@ def contacts(request):
 
 @login_required
 def edit_form(request):
+    """
+    Handles person form edition
+
+    If form is not valid and request.is_ajax - return json
+    dictionary that contains all errors. If request is not ajax (initial GET) -
+    simply render form with data from Person model.
+    """
     instance = Person.objects.all()[0]
     form = PersonForm(
         request.POST or None,
@@ -23,5 +31,8 @@ def edit_form(request):
     if form.is_valid():
         form.save()
         return redirect('contacts')
+    elif request.is_ajax():
+        errors = {'errors': dict(form.errors.items())}
+        return HttpResponse(json.dumps(errors), mimetype='application/json')
     else:
         return render(request, 'contact/edit_person_form.html', {'form': form})
